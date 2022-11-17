1b826e6d23bfed6a40856b94eff7f04c06e8f886
Admilson Ferreira <ferreiraad3@gmail.com>
2022-11-17 20:04:52 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
 import icon from '../../assets/img/notification-icon.svg';
-import './styles.css';
+import './styles.css'
 
 function NotificationButton() {
   
@@ -10,6 +10,6 @@ function NotificationButton() {
     )
   }
   
-  export default NotificationButton;
+export default NotificationButton;
   
 
